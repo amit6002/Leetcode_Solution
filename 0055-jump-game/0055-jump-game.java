8bb1614d76cf9e22355f0c8470ce0a3ba553265b
Amit Kumar <amitkumar.cse.dev@gmail.com>
2026-06-28 15:29:40 +0530

    Time: 2 ms (88.76%), Space: 48.1 MB (23.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxJumps=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxJumps) return false;
+            maxJumps=Math.max(maxJumps,i+nums[i]);
+        }
+        return true;
+    }
+}
