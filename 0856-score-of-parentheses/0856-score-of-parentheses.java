5435cab6bac8a46f0a7ccd4e3ca43dffacfa831c
Amit Kumar <amitkumar.cse.dev@gmail.com>
2026-07-25 20:38:49 +0530

    Time: 0 ms (100%), Space: 42.6 MB (85.9%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int curr = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                st.push(curr);
+                curr = 0;
+            } else {
+                curr = st.pop() + Math.max(2 * curr, 1);
+            }
+        }
+
+        return curr;
+    }
+}
